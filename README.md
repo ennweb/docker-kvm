e7e6aa40652af4644144dddad141119d8a189762
aemr3 <emres@petlantic.com>
2015-10-25 17:16:49 +0200

    Updated vnc options and network modes.

--- README.md
+++ README.md
@@ -4,50 +4,83 @@
 
 Boot with ISO
 ```
-docker run -v ${PWD}:/data \
-    --cap-add NET_ADMIN \
-    --device /dev/kvm:/dev/kvm \
-    --device /dev/net/tun:/dev/net/tun \
+docker run \
+    --privileged \
+    -v /dev:/dev \
+    -v ${PWD}:/data \
     -e RAM=2048 \
     -e SMP=1 \
     -e IMAGE=/data/disk-image \
-    -e ISO=http://releases.ubuntu.com/14.04.2/ubuntu-14.04.2-desktop-amd64.iso \
+    -e ISO=http://host/path/cd.iso \
+    -e ISO2=http://host/path/drivers.iso \
+    -e ISO_DOWNLOAD=1 \
+    -e VNC=tcp \
     -p 2222:22 \
     -p 8080:80 \
+    -p 5900:5900 \
     ennweb/kvm
 ```
 
 
 Boot with glusterfs volume
 ```
-docker run -v ${PWD}:/data \
-    --cap-add NET_ADMIN \
-    --device /dev/kvm:/dev/kvm \
-    --device /dev/net/tun:/dev/net/tun \
+docker run \
+    --privileged \
+    -v /dev:/dev \
     -e RAM=2048 \
     -e SMP=1 \
     -e IMAGE=gluster://server/volume/disk-image.qcow2 \
     -e ISO=gluster://server/volume/cd-image.iso \
+    -e VNC=tcp \
     -p 2222:22 \
     -p 8080:80 \
+    -p 5900:5900 \
     ennweb/kvm
 ```
 
 
 Create new volume file
 ```
-docker run -v ${PWD}:/data \
-    --cap-add NET_ADMIN \
-    --device /dev/kvm:/dev/kvm \
-    --device /dev/net/tun:/dev/net/tun \
+docker run \
+    --privileged \
+    -v /dev:/dev \
     -e RAM=2048 \
     -e SMP=1 \
     -e IMAGE=/data/disk-image \
     -e IMAGE_CREATE=1 \
+    -e VNC=tcp \
     -p 2222:22 \
     -p 8080:80 \
+    -p 5900:5900 \
     ennweb/kvm
 ```
 
 
-This image also provides vnc to access remote desktop via /data/vnc.socket file
+## Network modes
+
+`-e NETWORK=bridge --net=host -e NETWORK_BRIDGE=docker0 -e NETWORK_MAC=01:02:03:04:05`
+> Bridge mode will be enabled with docker0 interface. `--net=host` is required for this mode. Mac address is optional.
+
+`-e NETWORK=tap`
+> Enables NAT and port forwarding with tap device
+
+`-e NETWORK=macvtap --net=host -e NETWORK_IF=eth0 -e NETWORK_BRIDGE=vtap0 -e NETWORK_MAC=01:02:03:04:05`
+> Creates a macvtap device called vtap0 and will setup bridge with your external interface eth0. `--net=host` is required for this mode. Mac address is optional.
+
+`-e NETWORK=user -e PORTS=22,80`
+> Enables qemu user networking. Also redirects ports 22 and 80 to vm.
+
+
+## VNC options
+
+`-e VNC=tcp -e VNC_IP=127.0.0.1 -e VNC_ID=1`
+> VNC server will listen tcp connections on 127.0.0.1:5901
+
+`-e VNC=sock -e VNC_SOCK=/data/vnc.sock`
+> VNC server will listen on unix socket at /data/vnc.sock
+
+`-e VNC=reverse -e VNC_IP=1.1.1.1 -e VNC_PORT=5500`
+> Reverse VNC connection to 1.1.1.1:5500
+
+`-e VNC=none`
+> VNC server will be disabled
--- startup.sh
+++ startup.sh
@@ -29,7 +29,7 @@ if [ -n "$ISO" ]; then
   echo "[iso]"
   if [ "${ISO:0:1}" != "/" ] && [ "${ISO:0:10}" != "gluster://" ]; then
     basename=$(basename $ISO)
-    if [ ! -f "/data/${basename}" ] || [ "$ISO_FORCE_DOWNLOAD" != "0" ]; then
+    if [ ! -f "/data/${basename}" ] || [ "$ISO_DOWNLOAD" != "0" ]; then
       wget -O- "$ISO" > /data/${basename}
     fi
     ISO=/data/${basename}
@@ -46,7 +46,7 @@ if [ -n "$ISO2" ]; then
   echo "[iso2]"
   if [ "${ISO2:0:1}" != "/" ] && [ "${ISO2:0:10}" != "gluster://" ]; then
     basename=$(basename $ISO2)
-    if [ ! -f "/data/${basename}" ] || [ "$ISO_FORCE_DOWNLOAD" != "0" ]; then
+    if [ ! -f "/data/${basename}" ] || [ "$ISO_DOWNLOAD" != "0" ]; then
       wget -O- "$ISO2" > /data/${basename}
     fi
     ISO=/data/${basename}
@@ -69,79 +69,12 @@ FLAGS_DISK_IMAGE="-drive file=${IMAGE},if=virtio,cache=none,format=${IMAGE_FORMA
 echo "parameter: ${FLAGS_DISK_IMAGE}"
 
 echo "[network]"
-
-function cidr2mask() {
-  local i mask=""
-  local full_octets=$(($1/8))
-  local partial_octet=$(($1%8))
-
-  for ((i=0;i<4;i+=1)); do
-    if [ $i -lt $full_octets ]; then
-      mask+=255
-    elif [ $i -eq $full_octets ]; then
-      mask+=$((256 - 2**(8-$partial_octet)))
-    else
-      mask+=0
-    fi
-    test $i -lt 3 && mask+=.
-  done
-
-  echo $mask
-}
-
-function atoi {
-  IP=$1; IPNUM=0
-  for (( i=0 ; i<4 ; ++i )); do
-    ((IPNUM+=${IP%%.*}*$((256**$((3-${i}))))))
-    IP=${IP#*.}
-  done
-  echo $IPNUM
-}
-
-function itoa {
-  echo -n $(($(($(($((${1}/256))/256))/256))%256)).
-  echo -n $(($(($((${1}/256))/256))%256)).
-  echo -n $(($((${1}/256))%256)).
-  echo $((${1}%256))
-}
-
 if [ "$NETWORK" == "bridge" ]; then
-  IFACE=eth0
-  BRIDGE_IFACE=br0
-  MAC=`ip addr show $IFACE | grep ether | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*\$//g' | cut -f2 -d ' '`
-  IP=`ip addr show dev $IFACE | grep "inet " | awk '{print $2}' | cut -f1 -d/`
-  CIDR=`ip addr show dev $IFACE | grep "inet " | awk '{print $2}' | cut -f2 -d/`
-  NETMASK=`cidr2mask $CIDR`
-  GATEWAY=`ip route get 8.8.8.8 | grep via | cut -f3 -d ' '`
-  NAMESERVER=( `grep nameserver /etc/resolv.conf | cut -f2 -d ' '` )
-  NAMESERVERS=`echo ${NAMESERVER[*]} | sed "s/ /,/"`
-  dnsmasq --user=root \
-    --dhcp-range=$IP,$IP \
-    --dhcp-host=$MAC,$HOSTNAME,$IP,infinite \
-    --dhcp-option=option:router,$GATEWAY \
-    --dhcp-option=option:netmask,$NETMASK \
-    --dhcp-option=option:dns-server,$NAMESERVERS
+  NETWORK_BRIDGE="${NETWORK_BRIDGE:-docker0}"
   hexchars="0123456789ABCDEF"
-  end=$( for i in {1..8} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
-  NEWMAC=`echo 00:F0$end`
-  let "NEWCIDR=$CIDR-1"
-  i=`atoi $IP`
-  let "i=$i^(1<<$CIDR)"
-  NEWIP=`itoa i`
-  ip link set dev $IFACE down
-  ip link set $IFACE address $NEWMAC
-  ip addr del $IP/$CIDR dev $IFACE
-  brctl addbr $BRIDGE_IFACE
-  brctl addif $BRIDGE_IFACE $IFACE
-  ip link set dev $IFACE up
-  ip link set dev $BRIDGE_IFACE up
-  ip addr add $NEWIP/$NEWCIDR dev $BRIDGE_IFACE
-  if [[ $? -ne 0 ]]; then
-    echo "Failed to bring up network bridge"
-    exit 4
-  fi
-  echo allow $BRIDGE_IFACE > /etc/qemu/bridge.conf
-  FLAGS_NETWORK="-netdev bridge,br=${BRIDGE_IFACE},id=net0 -device virtio-net-pci,netdev=net0,mac=${MAC}"
+  NETWORK_MAC="${NETWORK_MAC:-$(echo 00:F0$(for i in {1..8} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g'))}"
+  echo allow $NETWORK_BRIDGE > /etc/qemu/bridge.conf
+  FLAGS_NETWORK="-netdev bridge,br=${NETWORK_BRIDGE},id=net0 -device virtio-net,netdev=net0,mac=${NETWORK_MAC}"
 elif [ "$NETWORK" == "tap" ]; then
   IFACE=eth0
   TAP_IFACE=tap0
@@ -160,15 +93,15 @@ elif [ "$NETWORK" == "tap" ]; then
   iptables -t nat -A POSTROUTING -o $IFACE -j MASQUERADE
   iptables -I FORWARD 1 -i $TAP_IFACE -j ACCEPT
   iptables -I FORWARD 1 -o $TAP_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -t nat -I PREROUTING -d $IP -p tcp -j DNAT --to-destination $NETWORK_IP
-  iptables -t nat -I PREROUTING -d $IP -p udp -j DNAT --to-destination $NETWORK_IP
+  if [ "$VNC" == "tcp" ]; then
+    vncport=5900+$VNC_ID
+    iptables -t nat -A PREROUTING -p tcp -d $IP ! --dport $vncport -j DNAT --to-destination $NETWORK_IP
+    iptables -t nat -A PREROUTING -p udp -d $IP -j DNAT --to-destination $NETWORK_IP
+    iptables -t nat -A PREROUTING -p icmp -d $IP -j DNAT --to-destination $NETWORK_IP
+  else
+    iptables -t nat -A PREROUTING -d $IP -j DNAT --to-destination $NETWORK_IP
+  fi
   FLAGS_NETWORK="-net nic,model=virtio -net tap,ifname=tap0,script=no"
-elif [ "$NETWORK" == "host" ]; then
-  NETWORK_BRIDGE="${NETWORK_BRIDGE:-docker0}"
-  hexchars="0123456789ABCDEF"
-  NETWORK_MAC="${NETWORK_MAC:-$(echo 00:F0$(for i in {1..8} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g'))}"
-  echo allow $NETWORK_BRIDGE > /etc/qemu/bridge.conf
-  FLAGS_NETWORK="-netdev bridge,br=${NETWORK_BRIDGE},id=net0 -device virtio-net,netdev=net0,mac=${NETWORK_MAC}"
 elif [ "$NETWORK" == "macvtap" ]; then
   NETWORK_IF="${NETWORK_IF:-eth0}"
   NETWORK_BRIDGE="${NETWORK_BRIDGE:-vtap0}"
@@ -199,8 +132,14 @@ echo "Using ${NETWORK}"
 echo "parameter: ${FLAGS_NETWORK}"
 
 echo "[Remote Access]"
-if [ -d /data ]; then
-  FLAGS_REMOTE_ACCESS="-vnc unix:/data/vnc.socket"
+if [ "$VNC" == "tcp" ]; then
+  FLAGS_REMOTE_ACCESS="-vnc ${VNC_IP}:${VNC_ID}"
+elif [ "$VNC" == "reverse" ]; then
+  FLAGS_REMOTE_ACCESS="-vnc ${VNC_IP}:${VNC_PORT},reverse"
+elif [ "$VNC" == "sock" ]; then
+  FLAGS_REMOTE_ACCESS="-vnc unix:${VNC_SOCK}"
+else
+  FLAGS_REMOTE_ACCESS=""
 fi
 echo "parameter: ${FLAGS_REMOTE_ACCESS}"
 
